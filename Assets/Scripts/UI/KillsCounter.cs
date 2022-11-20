0a5ef37e61ee6e974b19f5c2c0d54d41fc3a567a
Georgi Tsaklev <georgi.tsaklev@gmail.com>
2022-11-20 23:13:37 +0000

    Added UI Counter

---
+++ Assets/Scripts/UI/KillsCounter.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class KillsCounter : MonoBehaviour
+{
+    [SerializeField] private EnemyKilledSO enemyKilled;
+    [SerializeField] private TextMeshProUGUI text;
+
+    private void OnEnable() {
+        enemyKilled.killed += OnKill;
+
+        OnKill(enemyKilled.remaining);
+    }
+
+    private void OnDisable() {
+        enemyKilled.killed -= OnKill;
+    }
+
+    private void OnKill(int remaining) {
+        text.SetText(remaining.ToString("00.##"));
+    }
+}
