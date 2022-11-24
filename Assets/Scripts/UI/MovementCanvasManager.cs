749e8a01103ca113686951e660f6fe8a256564ca
Georgi Tsaklev <georgi.tsaklev@gmail.com>
2022-11-24 00:57:47 +0000

    Added initial controls display

---
+++ Assets/Scripts/UI/MovementCanvasManager.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementCanvasManager : MonoBehaviour
+{
+    private bool destroying = false;
+
+    private void OnEnable() {
+        InputManager.Instance.Player.Move.performed += OnMovement;
+    }
+
+    private void OnDisable() {
+        InputManager.Instance.Player.Move.performed -= OnMovement;
+    }
+
+    private void OnMovement(UnityEngine.InputSystem.InputAction.CallbackContext obj) {
+        if (destroying) return;
+
+        destroying = true;
+        Destroy(gameObject, 2);
+    }
+}
