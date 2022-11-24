6827dbbf000d3319db66d8b60b67464fd2750a92
Georgi Tsaklev <georgi.tsaklev@gmail.com>
2022-11-24 01:07:16 +0000

    Added level up effect

--- Assets/Scripts/Player/ExperienceManager.cs
+++ Assets/Scripts/Player/ExperienceManager.cs
@@ -6,12 +6,14 @@ public class ExperienceManager : MonoBehaviour
 {
     [SerializeField] private ExperienceSO playerExperience;
     [SerializeField] private InventorySO playerInventory;
+    [SerializeField] private GameObject levelUpPrefab;
 
     private void OnEnable() {
         playerExperience.change += OnExperienceChange;
     }
 
     private void OnExperienceChange(int level, bool levelUp, int current, int required) {
+            Instantiate(levelUpPrefab, transform);
         if (levelUp) {
             playerInventory.AddStone();
         }
