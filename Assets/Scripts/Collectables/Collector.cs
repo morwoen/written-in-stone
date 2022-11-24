13a5936e115886168fc833660b386fa5c211c3dc
Georgi Tsaklev <georgi.tsaklev@gmail.com>
2022-11-24 00:40:01 +0000

    Support expanding collectable radius around the player

--- Assets/Scripts/Collectables/Collector.cs
+++ Assets/Scripts/Collectables/Collector.cs
@@ -4,6 +4,28 @@ using UnityEngine;
 
 public class Collector : MonoBehaviour
 {
+    [SerializeField] private InventorySO inventory;
+
+    private float baseRadius;
+    private new SphereCollider collider;
+
+    private void Start() {
+        collider = GetComponent<SphereCollider>();
+        baseRadius = collider.radius;
+    }
+
+    private void OnEnable() {
+        inventory.effectsChange += OnEffectsChanged;
+    }
+
+    private void OnDisable() {
+        inventory.effectsChange -= OnEffectsChanged;
+    }
+    
+    private void OnEffectsChanged(List<InventorySO.ActiveInventorySlot> active, List<InventorySO.PassiveInventorySlot> passive) {
+        collider.radius = baseRadius * (1 + inventory.GetPassiveMultiplier(PassiveEffectSO.EffectProperty.PickupRange) / 100);
+    }
+
     private void OnTriggerEnter(Collider other) {
         var collectable = other.GetComponent<ICollectable>();
         collectable?.Collect(transform.parent);
