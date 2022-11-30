ec05623b7672a015ad736ec86e78a674c30491fe
tsukiall <tsukiall@gmail.com>
2022-11-30 18:51:33 +0000

    Add enemy attack sounds

--- Assets/Scripts/Enemies/EnemyAttack.cs
+++ Assets/Scripts/Enemies/EnemyAttack.cs
@@ -4,11 +4,23 @@ using UnityEngine;
 
 public class EnemyAttack : MonoBehaviour
 {
+    [SerializeField] AudioClip attackSound;
+
     protected int damage = 10;
     private bool damaged = false;
 
+    protected AudioSource audioSource;
+
+    protected void OnEnable() {
+        audioSource = GetComponent<AudioSource>();
+    }
+
     public void SetDamage(int damage) {
         this.damage = damage;
+    }
+
+    public void PlayAttackSound() {
+        audioSource.PlayOneShot(attackSound);
     }
 
     private void OnTriggerEnter(Collider collider){
