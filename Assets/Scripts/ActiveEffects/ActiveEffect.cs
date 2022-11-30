399d035a4ec1ddac6fc5089e4cfb4d758bb6e9d0
tsukiall <tsukiall@gmail.com>
2022-11-30 19:49:39 +0000

    Add bash sound

--- Assets/Scripts/ActiveEffects/ActiveEffect.cs
+++ Assets/Scripts/ActiveEffects/ActiveEffect.cs
@@ -4,14 +4,23 @@ using UnityEngine;
 
 public abstract class ActiveEffect : MonoBehaviour
 {
+    [SerializeField] private AudioClip effectSound;
+
     protected int damage = 1;
     protected float areaMultiplier = 1;
+
+    private AudioSource audioSource;
 
     public void SetParameters(int damage, float areaMultiplier) {
         this.damage = damage;
         this.areaMultiplier = areaMultiplier;
 
         UpdateGameObject();
+    }
+
+    public void PlayEffectSound() {
+        audioSource = GetComponent<AudioSource>();
+        audioSource.PlayOneShot(effectSound);
     }
 
     public abstract void UpdateGameObject();
